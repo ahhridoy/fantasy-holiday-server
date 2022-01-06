3f5342bd0ba0f601802748f19ed2930274c77280
ahhridoy <ahhridoyy28@gmail.com>
2022-01-06 21:26:32 +0600

    server updated

--- index.js
+++ index.js
@@ -14,70 +14,78 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s1xse.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
 async function run() {
-  try {
-    await client.connect();
-    const database = client.db("fantasyHoliday");
-    const servicesCollection = database.collection("services");
-    const ordersCollection = database.collection("orders");
+    try {
+        await client.connect();
+        const database = client.db("fantasyHoliday");
+        const servicesCollection = database.collection("services");
+        const placeOrdersCollection = database.collection("placeOrder");
 
-    // GET API
-    app.get("/services", async (req, res) => {
-      const cursor = servicesCollection.find({});
-      const services = await cursor.toArray();
-      res.send(services);
-    });
+        // GET API
+        app.get("/services", async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        });
 
-    app.get("/services/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const service = await servicesCollection.findOne(query);
-      res.send(service);
-    });
+        app.get("/services/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.send(service);
+        });
 
-    // ADD services
-    app.post("/addServices", async (req, res) => {
-      const newService = req.body;
-      const result = await servicesCollection.insertOne(newService);
-      res.json(result.insertedId);
-    });
+        // ADD services
+        app.post("/addServices", async (req, res) => {
+            const newService = req.body;
+            const result = await servicesCollection.insertOne(newService);
+            res.json(result.insertedId);
+        });
 
-    // POST API
-    app.post("/addOrder", async (req, res) => {
-      const newOrder = req.body;
-      const result = await ordersCollection.insertOne(newOrder);
-      res.json(result);
-    });
+        // get one product
+        app.get("/placeOrder/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const order = await servicesCollection.findOne(query);
+            res.send(order);
+        });
 
-    // GET my orders
-    app.get("/orders", async (req, res) => {
-      const cursor = ordersCollection.find({});
-      const orders = await cursor.toArray();
-      res.send(orders);
-    });
+        // post one product for place order
+        app.post("/placeOrder", async (req, res) => {
+            const placeOrder = req.body;
+            const result = await placeOrdersCollection.insertOne(placeOrder);
+            res.json(result);
+        });
 
-    // DELETE order
-    app.delete("/deleteOrder/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: id };
-      const result = await ordersCollection.deleteOne(query);
-      console.log(result)
-      res.json(result);
-    });
-  } finally {
-    // await client.close();
-  }
+        // get product by using email
+        app.get("/myOrders", async (req, res) => {
+            const cursor = placeOrdersCollection.find({});
+            const orders = await cursor.toArray();
+            res.send(orders);
+        });
+
+        // DELETE order
+        app.delete("/myOrders/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: id };
+            const result = await placeOrdersCollection.deleteOne(query);
+            console.log(result);
+            res.json(result);
+        });
+    } finally {
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Running my Crud Server");
+    res.send("Running my Crud Server");
 });
 
 app.listen(port, () => {
-  console.log("Running server on port", port);
+    console.log("Running server on port", port);
 });
